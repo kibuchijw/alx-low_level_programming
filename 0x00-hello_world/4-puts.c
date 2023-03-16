d8ca28c9a55df0cf9c317108fd8efa6ab710e115
kibuchijw <kibuchijw@gmail.com>
2023-03-16 18:48:23 +0000

    Write a C program that prints string with special character

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ *  main - Entry point
+ *   Description: 'the program printst the exact name without using printf'
+ *
+ *  Return: Always 0 (success)
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
