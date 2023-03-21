ba410fab4406962527750f0b231a20cd9a1ed8b5
kibuchijw <kibuchijw@gmail.com>
2023-03-21 14:09:12 +0000

    Print the alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * main - Entry point
+  * Description:'Print the alphabet in lowercase'
+  * Return: Always 0 (Success)
+  */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,3 @@
-#A repository meant to practice C-Functions, nested loops#
+A repository meant to practice C-Functions, nested loops
+0 - Print  _putchar_ followed by a new line
+1 - Print the alphabet in lowercase, followed by a new line
