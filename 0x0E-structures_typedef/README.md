e5d24f28d4faac7a2141f549b77ea9a2333aee76
kibuchijw <kibuchijw@gmail.com>
2023-04-18 07:56:23 +0000

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+The exercises herein are meant to practice C - Structures, typedef
+1. Define a new type struct dog
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+  * dog - Attributes of the dog
+  * @name: Dog name
+  * @age: Dog age
+  * @owner: Dog owner
+  *
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
