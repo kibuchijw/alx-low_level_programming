09e8e5e258f64848785438d3a15ece7a079fddfd
kibuchijw <kibuchijw@gmail.com>
2023-05-01 08:24:52 +0000

    Function that adds a new node at the beginning of a singly linked list

--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -1,3 +1,4 @@
 The exercises herein are meant to practice C - more singly linked lists
-1. 0-print_listint.c -  Function that prints all the elements of a listint_t list
+1. 0-print_listint.c - Function that prints all the elements of a listint_t list
 2. 1-listint_len.c - Function that Returns the number of elements in a listint_t list
+3. 2-add_nodeint.c - Function that adds a new node at the beginning of a listint_t list
---
+++ 0x13-more_singly_linked_lists/tests/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+  * add_nodeint - Adds a new node at the beginning of a listint_t list
+  * @head: Pointer to a pointer to the head of the list
+  * @n: Value to store in the new node
+  *
+  * Return: Address of the new element, or Null if it fails
+  */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node = NULL;
+
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
