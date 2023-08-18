362bcfb75786208d9f9591ba6a81dd4f2032e4de
kibuchijw <kibuchijw@gmail.com>
2023-08-18 02:36:00 +0000

    Fixed betty issues

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -29,12 +29,12 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 
 	else
-	{ 
+	{
 		tmp->prev->next = tmp->next;
 		if (tmp->next != NULL)
 			tmp->next->prev = tmp->prev;
 	}
 
-	free (tmp);
+	free(tmp);
 	return (1);
 }
